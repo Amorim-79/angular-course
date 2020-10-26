11160ef89ab9f3f65d4f68bcf87412bffc6df457
Pedro Henrique <pedro.henrique@helpper.com.br>
2020-10-25 23:01:38 -0300

    feature - Concluido as aulas de Event Biding, Style Biding e Property Biding e Interpolação

--- data-biding/src/app/app.module.ts
+++ data-biding/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DataBidingComponent } from './data-biding/data-biding.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { DataBidingComponent } from './data-biding/data-biding.component';
     DataBidingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- data-biding/src/app/data-biding/data-biding.component.ts
+++ data-biding/src/app/data-biding/data-biding.component.ts
@@ -7,7 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBidingComponent implements OnInit {
 
-  title = 'Data Biding';
+  url = 'http://loiane.com';
+  cursoAngular = true;
+  value = ['RxJs', 'Angular', 'ReactJS'];
+  urlImage = 'https://images.unsplash.com/photo-1603614989939-43d9c327c2eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80';
+  valorAtual: string;
+  valorSalvo: string;
+  isMouseOver = false;
+
+  getValor(): number {
+    return 1;
+  }
+
+  getCurtirCurso(): boolean {
+    return true;
+  }
+
+  botaoClicado(): void {
+    alert('Botão acionado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent): void {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+    console.log(this.valorAtual);
+  }
+
+  salvaValor(valor: string): void {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(): void {
+    this.isMouseOver = !this.isMouseOver;
+  }
 
   constructor() { }
 
