d6ff444e1753901959ac35ba0c61210df70be4a8
Pedro Henrique <pedro.henrique@helpper.com.br>
2021-01-05 00:59:50 -0300

    feature - Criado barra de progresso e operadores rxjs customizados

--- http/src/app/upload-file/upload-file/upload-file.component.ts
+++ http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,5 +1,9 @@
-import { environment } from '../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+
+import { filterResponse, uploadProgress } from '../../shared/rxjs-operators';
+
+import { environment } from '../../../environments/environment';
 
 import { UploadFileService } from './../upload-file.service';
 
@@ -11,6 +15,7 @@ import { UploadFileService } from './../upload-file.service';
 export class UploadFileComponent implements OnInit {
 
   files: Set<File>;
+  progress = 0;
 
   constructor(private uploadService: UploadFileService) { }
 
@@ -28,12 +33,27 @@ export class UploadFileComponent implements OnInit {
     });
     
     document.getElementById('customFileLabel').innerHTML = fileNames.join();
+
+    this.progress = 0;
   }
 
   onUpload(): void {
     if (this.files && this.files.size > 0) {
       this.uploadService.upload(this.files, environment.BASE_URL + '/upload')
-        .subscribe(response => console.log(response));
+        .pipe(
+          uploadProgress(progress => this.progress = progress),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload Concluído'));
+        // .subscribe((event: HttpEvent<Object>) => {
+        //   if (event.type === HttpEventType.Response) {
+        //     console.log('Upload Concluído');
+            
+        //   } else if (event.type === HttpEventType.UploadProgress) {
+        //     const percentDone = Math.round(event.loaded * 100 / event.total);
+        //     this.progress = percentDone;
+        //   }
+        // });
     }
   }
 
